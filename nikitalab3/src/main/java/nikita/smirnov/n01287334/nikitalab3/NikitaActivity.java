1374a011f21a681863e77e48543ca684426476eb
Nikita Smirnov <n01287334@humbermail.ca>
2020-11-26 17:26:58 -0500

    added HELP overflow item opening a link

--- nikitalab3/src/main/java/nikita/smirnov/n01287334/nikitalab3/NikitaActivity.java
+++ nikitalab3/src/main/java/nikita/smirnov/n01287334/nikitalab3/NikitaActivity.java
@@ -1,8 +1,14 @@
 package nikita.smirnov.n01287334.nikitalab3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class NikitaActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class NikitaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.themenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.item1:
+                setContentView(R.layout.webview);
+//                webView = (WebView) findViewById(R.id.webView1);
+//                webView.getSettings().setJavaScriptEnabled(true);
+//                webView.loadUrl("http://www.google.com");
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(browserIntent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
